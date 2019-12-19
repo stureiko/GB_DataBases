847b8466ab46985cbabe496c9d395a5580e64f7a
Igor Stureiko <stureiko@mail.ru>
2019-12-19 21:27:23 +0300

    Create table and add two columns. Add primary key - id and add index = id

---
+++ Lesson_2_hTask/Lesson_2.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE GeekBrainsDB;
+USE GeekBrainsDB;
+CREATE TABLE Users (
+    id integer NOT NULL PRIMARY KEY,
+    name char(50) NOT NULL,
+    surname char(50));
+CREATE UNIQUE INDEX UserID ON Users(id);
+
