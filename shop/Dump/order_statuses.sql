6e19b0cc2e28a65a123c012e573d125d98f7942b
Igor Stureiko <stureiko@mail.ru>
2020-02-17 10:20:24 +0300

    Изменены названия таблиц, включен дамп с правильными названиями статуса проекта

--- shop/Dump/order_statuses.sql
+++ shop/Dump/order_statuses.sql
@@ -19,16 +19,16 @@
 -- Table structure for table `order_statuses`
 --
 
--- DROP TABLE IF EXISTS `order_statuses`;
+DROP TABLE IF EXISTS `order_statuses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
--- CREATE TABLE `order_statuses` (
---  `status_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
---  `name` varchar(255) NOT NULL,
---  `description` text,
---  PRIMARY KEY (`status_id`),
---  UNIQUE KEY `name` (`name`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `order_statuses` (
+  `status_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`status_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,17 +37,7 @@
 
 LOCK TABLES `order_statuses` WRITE;
 /*!40000 ALTER TABLE `order_statuses` DISABLE KEYS */;
-INSERT INTO `order_statuses` VALUES 
-	(1,'processed','Order has been processed'),
-	(2,'confirm','Order has been confirmed'),
-	(3,'paid','Order has been paid'),
-	(4,'shipped','Order has been shipped'),
-	(5,'delivered','Order has been delivered'),
-	(6,'complited','Order has been complited'),
-	(7,'returned','Order has been returned'),
-	(8,'cancelled','Order has been cancelled'),
-	(9,'received','Order has been recieved'),
-	(10,'delayed','Order has been delayed');
+INSERT INTO `order_statuses` VALUES (1,'processed','Order has been processed'),(2,'confirm','Order has been confirmed'),(3,'paid','Order has been paid'),(4,'shipped','Order has been shipped'),(5,'delivered','Order has been delivered'),(6,'complited','Order has been complited'),(7,'returned','Order has been returned'),(8,'cancelled','Order has been cancelled'),(9,'received','Order has been recieved'),(10,'delayed','Order has been delayed');
 /*!40000 ALTER TABLE `order_statuses` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
